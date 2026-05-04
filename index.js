e007f933b52e4a9e502ee24c8ecc76bc644d06c5
shivamkosta <shivam.kosta@diverselynx.in>
2026-05-04 07:21:51 +0000

    add test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import request from "supertest";
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,6 +8,12 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     return res.json({message: "Hello from the server!"});
 })
+
+test("GET / should return message", async () => {
+  const res = await request(app).get("/");
+  expect(res.statusCode).toBe(200);
+  expect(res.body.message).toBe("Hello from the server!");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`);
